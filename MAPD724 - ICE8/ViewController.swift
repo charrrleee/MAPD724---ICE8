6da27ac8fb21d3850737f59b2f940cfa6310ab2b
charrrleee <charlcpy@gmail.com>
2023-03-21 20:05:48 -0400

    Initial Commit

---
+++ MAPD724 - ICE8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAPD724 - ICE8
+//
+//  Created by Charlene Cheung on 21/3/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
